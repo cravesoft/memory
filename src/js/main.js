ccd4ec1834a960fbd1dad65e77e62281855d7bfe
cravesoft <cravesoft@gmail.com>
2015-01-04 21:46:51 +0100

    Fixed localization on android

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
 'use strict';
 
-var i18n = require('i18next-client');
-
-i18n.init({
-    //lng: 'en-US',
-    ns: { namespaces: ['ns.common', 'ns.special'], defaultNs: 'ns.special'},
-    load: 'current',
-    getAsync: false,
-    useLocalStorage: false,
-    useCookie: false,
-    fallbackLng: false,
-    debug: true
-});
-
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'memory-game');
+var i18n = require('i18next-client')
+  , game = new Phaser.Game(1280, 720, Phaser.AUTO, 'memory-game');
 game.state.add('Boot',  require('./boot'));
 game.state.add('Preloader', require('./preloader'));
 game.state.add('Menu', require('./menu'));
 game.state.add('Game', require('./game'));
 
-game.state.start('Boot');
+var lang = (navigator.language.split('-')[0]);
+
+i18n.init({
+    lng: lang,
+    lngWhitelist: ['en', 'fr'],
+    ns: { namespaces: ['ns.common', 'ns.special'], defaultNs: 'ns.special'},
+    load: 'current',
+    getAsync: true,
+    useLocalStorage: false,
+    useCookie: false,
+    fallbackLng: 'en',
+    debug: true
+}).done( function() {
+    game.state.start('Boot');
+});
